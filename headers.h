f9b68dd318f812689981c82e1eaa1057070b12e3
athompson00 <athompson00@stuy.edu>
2019-10-16 22:01:48 -0400

    fixed some stuff I think

--- headers.h
+++ headers.h
@@ -1,4 +1,4 @@
 struct student creation();
-struct student {char name[15]; int GPA;};
+struct student {char *name; int GPA;};
 void print(struct student s);
 void change(struct student s, char name[15], int GPA);
